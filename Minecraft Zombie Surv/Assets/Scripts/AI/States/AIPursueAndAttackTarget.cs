5d53019aeaf8cb0d8fcfacddf9e3ef3940df3b33
Asigaka <AsigakaGamedev@gmail.com>
2023-12-14 23:53:26 +0300

    ai pursue target and attack

--- Minecraft Zombie Surv/Assets/Scripts/AI/States/AIPursueAndAttackTarget.cs
+++ Minecraft Zombie Surv/Assets/Scripts/AI/States/AIPursueAndAttackTarget.cs
@@ -1,18 +1,51 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class AIPursueAndAttackTarget : MonoBehaviour
+public class AIPursueAndAttackTarget : AIStateBase
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private NavMeshAgent agent;
+
+    [Space]
+    [SerializeField] private float pursueSpeed;
+    [SerializeField] private float pursueStoppingDistance;
+
+    [Space]
+    [SerializeField] private LayerMask targetsLayer;
+    [SerializeField] private float targetsCheckRadius;
+
+    [Space]
+    [ReadOnly, SerializeField] private Collider target;
+
+    public override bool OnValidateState()
     {
-        
+        Collider[] colliders = Physics.OverlapSphere(transform.position, targetsCheckRadius, targetsLayer);
+
+        if (colliders.Length > 0)
+        {
+            target = colliders[0];
+            return true;
+        }
+
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnEnterState()
     {
-        
+        base.OnEnterState();
+
+        agent.speed = pursueSpeed;
+        agent.stoppingDistance = pursueStoppingDistance;
+    }
+
+    public override void OnUpdateState()
+    {
+        base.OnUpdateState();
+
+        if (!target) return;
+
+        agent.SetDestination(target.transform.position);
     }
 }
