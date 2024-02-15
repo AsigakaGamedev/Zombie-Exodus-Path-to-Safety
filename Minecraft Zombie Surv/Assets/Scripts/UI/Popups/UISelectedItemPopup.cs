6ffe4a5507698af8ed965d67086a5336e9e6656c
Asigaka <AsigakaGamedev@gmail.com>
2024-02-15 14:37:22 +0300

    ui selected item popup set item info

--- Minecraft Zombie Surv/Assets/Scripts/UI/Popups/UISelectedItemPopup.cs
+++ Minecraft Zombie Surv/Assets/Scripts/UI/Popups/UISelectedItemPopup.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Services.Economy.Model;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UISelectedItemPopup : APopup
 {
+    [SerializeField] private Image itemIcon; 
+    [SerializeField] private TextMeshProUGUI itemName; 
+    [SerializeField] private TextMeshProUGUI itemDesc; 
+
+    [Space]
     [SerializeField] private Button useItemBtn;
     [SerializeField] private Button equipItemBtn;
     [SerializeField] private Button dropItemBtn;
 
     private UIPopupsManager popupsManager;
+    private LocalizationManager localizationManager;
     private ItemEntity selectedItem;
 
     public override void OnInit()
@@ -18,6 +25,7 @@ public class UISelectedItemPopup : APopup
         base.OnInit();
 
         popupsManager = ServiceLocator.GetService<UIPopupsManager>();
+        localizationManager = ServiceLocator.GetService<LocalizationManager>();
 
         useItemBtn.onClick.AddListener(() =>
         {
@@ -27,6 +35,7 @@ public class UISelectedItemPopup : APopup
 
         equipItemBtn.onClick.AddListener(() =>
         {
+            selectedItem.EquipItem();
             popupsManager.CloseCurrentPopup();
         });
 
@@ -40,7 +49,14 @@ public class UISelectedItemPopup : APopup
     {
         selectedItem = item;
 
-        useItemBtn.gameObject.SetActive(item.InfoPrefab.CanUse);
-        equipItemBtn.gameObject.SetActive(item.InfoPrefab.CanEquip);
+        ItemInfo info = item.InfoPrefab;
+
+        itemIcon.sprite = info.ItemCellSprite;
+
+        itemName.text = localizationManager.CurrentLocalization.GetValue(info.ItemNameKey);
+        itemDesc.text = localizationManager.CurrentLocalization.GetValue(info.ItemDescriptionKey);
+
+        useItemBtn.gameObject.SetActive(info.CanUse);
+        equipItemBtn.gameObject.SetActive(info.CanEquip);
     }
 }
